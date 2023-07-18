9449311af545f1b9c1fbd22df0caf194ad0ed5a6
Aero Hyena <rakudreemurr@gmail.com>
2023-07-18 05:59:51 +0000

    Updated layout and comments on test.py

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+"""
+This is a Python script built to explore text extraction from a pdf file.
+
+This is done by converting the pdf file to images, 
+and then using ocr technolies on the images to extract the text.
+"""
 from PIL import Image
 
 # pdf2image
@@ -7,21 +13,31 @@ from pdf2image import convert_from_bytes
 import pytesseract
 
 
-# Simple image to string
+
+
+#### Read the text on an test.png and print it to console
 print(pytesseract.image_to_string(Image.open('test.png')))
+
+# Line to seperate file contents in console
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
-# Convert pdf to image
-output_filename = "f"
 
+#### Convert pdf to image, and read its contents
+OUTPUT_FILENAME = "f"
+
+# Convert pdf to images.
+# Images are saved in the output folder in jpeg format.
+# Only the first two pages of the pdf is read.
 images = convert_from_bytes(
     open('dummy.pdf', 'rb').read(),
     output_folder='output',
     fmt='jpeg',
     first_page=1,
     last_page=2,
-    output_file=output_filename)
+    output_file=OUTPUT_FILENAME)
 
-print(pytesseract.image_to_string(f'output/{output_filename}0001-1.jpg'))
+# Read the image files in the output folder,
+# and print the contents to console.
+print(pytesseract.image_to_string(f'output/{OUTPUT_FILENAME}0001-1.jpg'))
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-print(pytesseract.image_to_string(f'output/{output_filename}0001-2.jpg'))
+print(pytesseract.image_to_string(f'output/{OUTPUT_FILENAME}0001-2.jpg'))
