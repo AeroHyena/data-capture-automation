acbb0a5a892956fc9739737370d6d6443fec7395
Aero Hyena <rakudreemurr@gmail.com>
2023-08-16 06:22:55 +0000

    implemented database functionality in apply_data.py by creating a DatabaseManager object, and implemented said object into main.py

--- app/apply_data.py
+++ app/apply_data.py
@@ -1,15 +1,85 @@
 """
-This module is responsible for taking given data points
-and applying it to the data capture.
-
-It will also create an email with text and files auto-attached,
-but will not send the email so that the user can check the email before sending.
-
-The primary way of applying data is using pyautogui
-to control the mouse and keyboard.
+This module is responsible for accessing the MySQL
+database to add and retrieve entries.
 """
 
-# import pyautogui
+import mysql.connector
 
-# Temp code - get XY coordinates of the mouse
-# pos = pyautogui.position()
+
+class DatabaseManager:
+    def __init__(self, server: dict):
+        """
+        Initialize the DatabaseManager instance.
+
+        Parameters:
+            server (dict): a dictionary containing database configuration parameters.
+                           The dictionary should have keys 'host', 'user', 'password' and 'database'.
+        """ 
+        self.server = server
+        # Check if the required server data is provided
+        if not (server["host"] and server["port"] and server["user"] and server["password"] and server["database"]):
+            raise ValueError("Please provide the host, port, user, password and database keys witin your server dictionary.")
+        self.connection = None
+        self.cursor = None
+
+
+    def connect(self):
+        # connect to the database
+        self.connection = mysql.connector.connect(
+            host=self.server["host"],
+            port=self.server["port"],
+            user=self.server["user"],
+            password=self.server["password"],
+            database=self.server["database"]
+        )
+        print(f"connected to database at {server['host']}:{server['port']} as {server['user']}")
+
+        # create the cursor variable for the execution of queries
+        self.cursor = self.connection.cursor()
+        print("created cursor for database connection")
+    
+
+    def close_connection(self):
+        if self.connection:
+            self.connection.close()
+            print("The connection to the database has been closed.")
+
+
+    def insert_data(data):
+        """
+        Create a new data entry into the database using the
+        provided data. 
+        Returns the unique ID of the data entry
+        """
+        insert_query = f"{data}"
+    
+        self.cursor.execute(insert_query)
+        
+        document_id = cursor.lastrowid
+
+        print(f"data created at document no {document_id}")
+        return document_id
+
+
+
+    def retrieve_data(document_id):
+        """
+        Retrieve data using the specified ID.
+        """
+        query = f"SELECT * FROM weighbridge_data WHERE document_id = {document_id}"
+        delf.cursor.execute(query)
+        data = cursor.fetchone()
+
+        print(f"data retrieved for document no {document_id}")
+        return data
+
+
+    def rerieve_last_entry():
+        """
+        Retrieve the ID fof the most recent data inserted
+        """
+        query = "SELECT * FROM weighbrighe_data ORDER BY created_at DESC LIMIT 1"
+        self.cursor.execute(query)
+        
+        return self.cursor.fetchone()
+        
--- app/main.py
+++ app/main.py
@@ -2,15 +2,26 @@
 This script launches the app and its gui interfaces.
 All the elements of the gui is defined and outlined on this script.
 """
+
 # Kivy
 # from kivy.app import App
 # from kivy.uix.label import Label
 
 # Local modules
 # import pdf_ocr
-# import apply_data
+from apply_data import DatabaseManager
 
 
+## Define your server keys as a dictionary and create a DatabaseManager class instance
+server = {
+    "port": 9005,
+    "host": "10.101.85.25",
+    "user": "root",
+    "password": "123456", 
+    "database": "mysqldb"  
+}
+
+database = DatabaseManager(server)
 
 ## Create the main app and its gui interface and functionalities
 # class MainApp(App):
@@ -18,7 +29,7 @@ All the elements of the gui is defined and outlined on this script.
     This is the main class of the gui interface.
     It is responsible for the gui and all its functionalities.
     
-    """
+"""
 
     # def build(self):
         # Put a temporary placeholder text on the gui interface
@@ -26,6 +37,12 @@ All the elements of the gui is defined and outlined on this script.
 
 
 
-## On app launch, initialize the gui interface
-# if __name__ == '__main__':
+## On app launch, initialize the gui interface and establish a connection to the database
+if __name__ == '__main__':
     # MainApp().run()
+
+    try:
+        database.connect()
+    finally:
+        database.close_connection()
+
